702e23a3de57424dbb0c3ca31b5a0415e494a24e
Marchelino Gifensius <100520193+MGifensius@users.noreply.github.com>
2025-11-14 02:34:05 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,7 +22,7 @@ module.exports = {
         },
       },
       fontFamily: {
-        sans: ["var(--font-inter)", "system-ui", "sans-serif"],
+        sans: ["var(--font-poppins)", "system-ui", "sans-serif"],
       },
     },
   },
